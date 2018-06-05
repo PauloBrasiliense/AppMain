e5ccf2154862ba53d10f8b6e6d585abe6d33f9b9
PauloBrasiliense <pjbrasiliense@hotmail.com>
2018-06-04 21:12:01 -0300

    task 05

--- src/pages/vaga/vaga.ts
+++ src/pages/vaga/vaga.ts
@@ -12,11 +12,32 @@ export class VagaPage {
 
 
   knobValues: any = {
-
     upper: 0,
-
     lower: 0
-
+  }
+  hSeg: any = {
+    upper: 0,
+    lower: 0
+  }
+  hTer: any = {
+    upper: 0,
+    lower: 0
+  }
+  hQua: any = {
+    upper: 0,
+    lower: 0
+  }
+  hQui: any = {
+    upper: 0,
+    lower: 0
+  }
+  hSex: any = {
+    upper: 0,
+    lower: 0
+  }
+  hSab: any = {
+    upper: 0,
+    lower: 0
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
